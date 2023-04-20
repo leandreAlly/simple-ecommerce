4229be6b42fa6dd319c07e300551db20142059d1
allyleandre <tuyambazeleandre@gmail.com>
2023-04-20 21:33:24 +0200

    Build the hover button add to cart

--- src/components/Product.js
+++ src/components/Product.js
@@ -21,6 +21,28 @@ const Product = ({ product }) => {
             />
           </div>
         </div>
+        {/* button */}
+        <div
+          className='absolute top-6 right-11 group-hover:right-5 p-2 
+                     flex flex-col items-center justify-center gap-y-2 opacity-0 
+                     group-hover:opacity-100 transition-all duration-300'
+        >
+          <button>
+            <div
+              className='flex justify-center items-center 
+                            text-white w-12 h-12 bg-red-500'
+            >
+              <BsPlus className='text-3xl' />
+            </div>
+          </button>
+          <Link
+            to={"/"}
+            className='w-12 h-12 bg-white flex justify-center items-center
+             text-primary drop-shadow-xl'
+          >
+            <BsEyeFill />
+          </Link>
+        </div>
       </div>
       <div>2</div>
     </div>
