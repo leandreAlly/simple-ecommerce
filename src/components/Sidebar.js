88fb9848f6ffc951adae6b65c0d11183b8ae2d41
allyleandre <tuyambazeleandre@gmail.com>
2023-04-25 19:39:28 +0200

    clearing cart

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,7 +9,7 @@ import { CartContext } from "../contexts/CartContext";
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
-  const { cart } = useContext(CartContext);
+  const { cart, clearCart } = useContext(CartContext);
 
   return (
     <div
@@ -31,6 +31,20 @@ const Sidebar = () => {
         {cart.map((item) => {
           return <CartItem item={item} key={item.id} />;
         })}
+      </div>
+      <div>
+        <div className='bg-pink-200 flex w-full justify-between items-center '>
+          <div>
+            <span>Total:</span>$1000
+          </div>
+          <div
+            onClick={clearCart}
+            className='cursor-pointer py-4 bg-red-500 text-white
+         w-12 h-12 flex justify-center items-center text-xl'
+          >
+            <FiTrash2 />
+          </div>
+        </div>
       </div>
     </div>
   );
